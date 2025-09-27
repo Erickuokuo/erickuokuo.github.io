62596af236757ddbf2e944f0d91365e4211cfa23
Eric Kuo <eric_kuo@outlook.com>
2025-09-27 16:15:44 -0400

    new icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -56,6 +56,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <link rel="icon" type="image/png" href="/icon.png" />
       <body
         className={cn(
           "min-h-screen bg-background font-sans antialiased max-w-2xl mx-auto py-12 sm:py-24 px-6",
